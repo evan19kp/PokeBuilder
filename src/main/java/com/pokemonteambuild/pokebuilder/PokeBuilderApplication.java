f1ce6b0d35624e7d639b120517004a06f057f79b
Evan Packard <evanpacpac@outlook.com>
2026-09-01 01:13:53 -0700

    first

---
+++ src/main/java/com/pokemonteambuild/pokebuilder/PokeBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.pokemonteambuild.pokebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeBuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokeBuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pokemonteambuild/pokebuilder/PokeBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokemonteambuild.pokebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeBuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
